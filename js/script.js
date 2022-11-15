bec8fae3cb35a0c2bacac3ebe485f98bb17a9cd4
Bhavya Grover <bgrover4@my.bcit.ca>
2022-11-15 14:54:56 -0800

    Function activity

---
+++ js/script.js
@@ -0,0 +1,17 @@
+// function greet(user){
+//     console.log(`Hello ${user}!`);
+// }
+
+// greet("Bhavya");
+
+
+// let greet= function(user){
+//     console.log(`Hello ${user}!`);
+// }
+
+const greet=(user,time="day")=>
+{
+    console.log(`Good ${time} ${user}`);
+}
+greet("Neda","night","morning");
+greet("Neda");
